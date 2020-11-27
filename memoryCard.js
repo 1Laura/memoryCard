d534f08c9e21762e1f47ad0373827661837cf346
Laura Baltrukonė <baltrukone@gmail.com>
2020-11-27 09:42:54 +0200

    card

--- memoryCard.js
+++ memoryCard.js
@@ -1,48 +1,69 @@
 const memoryGame = document.querySelector('.memoryGame');
 // const cards = document.querySelectorAll('.memoryCard');
 
-let colorArray = ['#fd4508', '#457300', '#d604f8', '#ca0505', '#eed00f', '#0036cb']
+// let colorArray = ['#fd4508', '#457300', '#d604f8', '#ca0505', '#eed00f', '#0036cb']
 
-// '#fd4508', '#457300', '#d604f8', '#ca0505', '#eed00f', '#0036cb']
+colorsArray = []
 
 function createCard() {
+
     for (let i = 0; i < 12; i++) {
-        // let randomColor = Math.floor(Math.random() * colorArray.length)
-        let random = Math.floor(Math.random() * colorArray.length)
-        console.log(random)
-        let memoryCard = document.createElement("div")
-        memoryCard.classList.add('memoryCard')
-        memoryCard.addEventListener('click', flipCard)
+        let randomIndex = Math.floor(Math.random() * colorsArray.length)
+        memoryGame.innerHTML += `
+        <div class="${colors[randomIndex]}" onclick="flipCard(event)"></div>`
 
-        let frontFace = document.createElement("div")
-        frontFace.classList.add('frontFace')
-
-        frontFace.classList.add(colorArray[random])
+        // let memoryCard = document.createElement("div")
+        // memoryCard.classList.add('memoryCard')
         // memoryCard.addEventListener('click', flipCard)
+        //
+        // let frontFace = document.createElement("div")
+        // frontFace.classList.add('frontFace')
+        //
+        // frontFace.classList.add(colorArray[random])
+        // // memoryCard.addEventListener('click', flipCard)
+        //
+        // let backFace = document.createElement("div")
+        // backFace.classList.add('backFace')
+        // // backFace.addEventListener('click', flipCard)
+        //
+        // memoryCard.appendChild(backFace)
+        // memoryCard.appendChild(frontFace)
+        // memoryGame.appendChild(memoryCard)
+        // colorsArray = colorsArray.filter((color, index) => index !== randomIndex)
+        colorsArray.splice(randomIndex, 1)
+    }
 
-        let backFace = document.createElement("div")
-        backFace.classList.add('backFace')
-        // backFace.addEventListener('click', flipCard)
-
-        memoryCard.appendChild(backFace)
-        memoryCard.appendChild(frontFace)
-        memoryGame.appendChild(memoryCard)
-
-
-        // console.log(memoryCard)
+    //colors
+    for (let i = 0; i < 6; i++) {
+        let randomColor = "#" + ((1 << 24) * Math.random() | 0).toString(16)
+        colors.push(rnd, rnd)
     }
 }
 
 createCard()
 
+let clickedCard = {
+    one: null,
+    two: null,
+    counter: 0
+}
 
 function flipCard(event) {
+    let color = event.target.className
+    event.target.style.backgroundColor = color
 
-    let randomColor = Math.floor(Math.random() * colorArray.length)
+    if (clickedCard.counter === 0) {
+        clickedCard.one = event.target
+        clickedCard.one.style.backgroundColor = color
+        return clickedCard.counter++
+    }
+    if (clickedCard.counter === 1) {
+        clickedCard.two = event.target
+        clickedCard.two.style.backgroundColor = color
+        return clickedCard.counter++
+    }
 
-    // event.path[0] =
-    event.target.backgroundColor = randomColor.value
-    // console.log(event.target.backgroundColor)
+
 }
 
 
