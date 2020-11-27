de3ecb4d52e9a64ada346b23e6f6492faeaa24b2
Laura Baltrukonė <baltrukone@gmail.com>
2020-11-27 10:08:18 +0200

    flip card

--- memoryCard.js
+++ memoryCard.js
@@ -5,8 +5,17 @@ const memoryGame = document.querySelector('.memoryGame');
 
 colorsArray = []
 
-function createCard() {
+createCard()
 
+
+function createCard() {
+//colors
+    for (let i = 0; i < 6; i++) {
+        let randomColor = "#" + ((1 << 24) * Math.random() | 0).toString(16)
+        colorsArray.push(randomColor, randomColor)
+    }
+
+    //html card
     for (let i = 0; i < 12; i++) {
         let randomIndex = Math.floor(Math.random() * colorsArray.length)
         memoryGame.innerHTML += `
@@ -29,18 +38,12 @@ function createCard() {
         // memoryCard.appendChild(backFace)
         // memoryCard.appendChild(frontFace)
         // memoryGame.appendChild(memoryCard)
+
         // colorsArray = colorsArray.filter((color, index) => index !== randomIndex)
         colorsArray.splice(randomIndex, 1)
     }
-
-    //colors
-    for (let i = 0; i < 6; i++) {
-        let randomColor = "#" + ((1 << 24) * Math.random() | 0).toString(16)
-        colors.push(rnd, rnd)
-    }
 }
 
-createCard()
 
 let clickedCard = {
     one: null,
@@ -52,12 +55,12 @@ function flipCard(event) {
     let color = event.target.className
     event.target.style.backgroundColor = color
 
-    if (clickedCard.counter === 0 && !event.target.classList.contains('clicked')) {
+    if (clickedCard.counter === 0 && !event.target.classList.contains('flip')) {
         clickedCard.one = event.target
         clickedCard.one.style.backgroundColor = color
         return clickedCard.counter++
     }
-    if (clickedCard.counter === 1 && !event.target.classList.contains('clicked')) {
+    if (clickedCard.counter === 1 && !event.target.classList.contains('flip')) {
         clickedCard.two = event.target
         clickedCard.two.style.backgroundColor = color
         checkForMatch()
@@ -67,118 +70,16 @@ function flipCard(event) {
 
 function checkForMatch() {
     if (clickedCard.one.className === clickedCard.two.className) {
-        clickedCard.one.classList.add('clicked')
-        clickedCard.two.classList.add('clicked')
+        clickedCard.one.classList.add('flip')
+        clickedCard.two.classList.add('flip')
         clickedCard.counter = 0
     } else {
         setTimeout(() => {
-            clickedCard.one.classList.remove('clicked')
-            clickedCard.two.classList.remove('clicked')
+            clickedCard.one.classList.remove('flip')
+            clickedCard.two.classList.remove('flip')
             clickedCard.one.style.backgroundColor = 'white'
             clickedCard.two.style.backgroundColor = 'white'
             clickedCard.counter = 0
         }, 1500)
     }
 }
-
-
-// function makeSquares()  {
-// //     for (let x = 0; x < 12; x++) {
-//         let randomColor = Math.floor(Math.random() * colors.length)
-//         container.innerHTML += `<div class="${colors[randomColor]}" onclick="boxClicked(${x})"></div>`
-//         // container.innerHTML += `<div class="${colors[randomColor]}" onclick="boxClicked(${x})"></div>`
-//         colors = colors.filter((item, index) => index != randomColor)
-//         console.log(colors[randomColor])
-//     }
-// }
-
-
-////// pvz
-// const cards = document.querySelectorAll('.memory-card');
-
-// let hasFlippedCard = false;
-// let lockBoard = false;
-// let firstCard, secondCard;
-//
-// function flipCard() {
-//     if (lockBoard) return;
-//     if (this === firstCard) return;
-//     this.classList.add('flip');
-//     if (!hasFlippedCard) {
-//         hasFlippedCard = true;
-//         firstCard = this;
-//         return;
-//     }
-//     secondCard = this;
-//     checkForMatch();
-// }
-//
-// function checkForMatch() {
-//     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-//     isMatch ? disableCards() : unflipCards();
-// }
-//
-// function disableCards() {
-//     firstCard.removeEventListener('click', flipCard);
-//     secondCard.removeEventListener('click', flipCard);
-//     resetBoard();
-// }
-//
-// function unflipCards() {
-//     lockBoard = true;
-//     setTimeout(() => {
-//         firstCard.classList.remove('flip');
-//         secondCard.classList.remove('flip');
-//         resetBoard();
-//     }, 1500);
-// }
-//
-// function resetBoard() {
-//     [hasFlippedCard, lockBoard] = [false, false];
-//     [firstCard, secondCard] = [null, null];
-// }
-//
-// (function shuffle() {
-//     cards.forEach(card => {
-//         let randomPos = Math.floor(Math.random() * 12);
-//         card.style.order = randomPos;
-//     });
-// })();
-//
-// cards.forEach(card => card.addEventListener('click', flipCard));
-
-
-//////////////mano
-// let container = document.getElementById('container')
-// let colors = [blue, ]
-//
-// function makeSquares() {
-//     for (let x = 0; x < 12; x++) {
-//         let randomColor = Math.floor(Math.random() * colors.length)
-//         container.innerHTML += `<div class="${colors[randomColor]}" onclick="boxClicked(${x})"></div>`
-//         // container.innerHTML += `<div class="${colors[randomColor]}" onclick="boxClicked(${x})"></div>`
-//         colors = colors.filter((item, index) => index != randomColor)
-//         console.log(colors[randomColor])
-//     }
-// }
-//
-// generateColor()
-// makeSquares()
-//
-// function boxClicked(event, x) {
-// //     let boxColor = event
-//     console.log(event.target.classList)
-//     // console.log(colors)
-//     // console.log(x)
-// }
-//
-// function generateColor() {
-//     for (let i = 0; i < 6; i++) {
-//         let randomColor = "#" + ((1 << 24) * Math.random() | 0).toString(16)
-//         console.log(randomColor)
-//         colors.push(randomColor)
-//     }
-//     colors = [...colors, ...colors]
-//     // console.log(colors)
-// }
-
